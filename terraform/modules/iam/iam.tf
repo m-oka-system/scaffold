a29d125263c20f1dcc79b9b2f5e9f1527c7c7277
m-oka-system <m-oka-system@outlook.com>
2021-07-21 21:28:33 +0900

    [clean]IAMポリシーをJSON参照に変更

--- terraform/modules/iam/iam.tf
+++ terraform/modules/iam/iam.tf
@@ -1,21 +1,7 @@
 ################################
 # IAM
 ################################
-data "aws_iam_policy" "administrator" {
-  arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-data "aws_iam_policy_document" "ec2_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
+# EC2
 resource "aws_iam_instance_profile" "ec2" {
   name = aws_iam_role.ec2.name
   role = aws_iam_role.ec2.name
@@ -23,10 +9,10 @@ resource "aws_iam_instance_profile" "ec2" {
 
 resource "aws_iam_role" "ec2" {
   name               = "ec2-admin-role"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+  assume_role_policy = file("../../modules/iam/policies/ec2_assume_role_policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "ec2" {
   role       = aws_iam_role.ec2.name
-  policy_arn = data.aws_iam_policy.administrator.arn
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
