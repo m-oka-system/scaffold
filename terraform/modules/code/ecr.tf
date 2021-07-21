e26b85305d106410f885943b4a83c8aa723ef65d
m-oka-system <m-oka-system@outlook.com>
2021-07-21 22:28:24 +0900

    [add]ECRを追加

---
+++ terraform/modules/code/ecr.tf
@@ -0,0 +1,11 @@
+################################
+# ECR
+################################
+resource "aws_ecr_repository" "this" {
+  name                 = "sccafold"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
--- terraform/modules/code/output.tf
+++ terraform/modules/code/output.tf
@@ -1,3 +1,7 @@
-output "clone_url_http" {
+output "codecommit_repository" {
   value = aws_codecommit_repository.this.clone_url_http
+}
+
+output "ecr_repository" {
+  value = aws_ecr_repository.this.repository_url
 }
