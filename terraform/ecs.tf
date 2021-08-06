d02ccd7db0098b6249c6cfb3b0846c2b6cbe38b1
m-oka-system <m-oka-system@outlook.com>
2021-08-06 23:02:31 +0900

    [add]ECSサービスを追加

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -76,3 +76,24 @@ resource "aws_ecs_task_definition" "app" {
     ]
   )
 }
+
+# Service
+resource "aws_ecs_service" "this" {
+  name            = "${var.env}-${var.project}-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.this.arn
+    container_name   = "app"
+    container_port   = "3000"
+  }
+
+  network_configuration {
+    subnets          = aws_subnet.public.*.id
+    security_groups  = [aws_security_group.app.id]
+    assign_public_ip = true
+  }
+}
